f5fdf1ab791b8191176114caa145d7f403309961
Biilge <biilgesena@gmail.com>
2025-10-28 23:05:15 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void sayHello(char name[]) {
+    printf("Hello, %s!\n", name);
+}
+
+int main() {
+    char name[20];  
+
+    printf("Enter your name: ");
+    scanf("%s", name);  
+
+    sayHello(name);  
+
+    return 0;
+}
 
