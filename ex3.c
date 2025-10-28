34522e91506b0a1487f19651c2053b4c8d1d903c
Biilge <biilgesena@gmail.com>
2025-10-28 23:03:59 +0300

    Implement pet type input and response
    Added user input for pet type and responses.

--- ex3.c
+++ ex3.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char pet[10];
+      printf("Do you have cat or dog?");
+      scanf("%s", pet);
+
+
+    if (strcmp(pet, "cat") == 0) {
+        printf("Meow!.\n");
+    }
+    else if (strcmp(pet, "dog") == 0){
+        printf("Hav!.\n");
+    }
+    return 0;
+}
 
