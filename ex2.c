585820fff316354379198d1229ebaceadd63f385
Biilge <biilgesena@gmail.com>
2025-10-28 23:03:18 +0300

    Add age prompt and infinite birthday message
    The program now prompts the user for their age and prints 'Happy Birthday!' indefinitely.

--- ex2.c
+++ ex2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int number;
+    printf("\nHow old are you?: ");
+    scanf("%d", &number);
+
+    for (;;) { 
+        printf("Happy Birthday!\n");
+    }
+
+    return 0;
+}
