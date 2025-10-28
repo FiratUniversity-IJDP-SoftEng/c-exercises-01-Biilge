f04e4aaa894f3a480369fa0e55a1dc59bceb8b25
Biilge <biilgesena@gmail.com>
2025-10-28 23:02:20 +0300

    Add number guessing game in ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("\nGuess one of the numbers (1), (2), (3), (4) or (5): ");
+    scanf("%d", &number);
+
+    if (number == 3) {
+        printf("\nCongratulations, you guessed correctly!\n");
+    }
+    else if (number == 1 || number == 2 || number == 4 || number == 5) {
+        printf("\nSorry, that's wrong. Try again.\n");
+        scanf("%d", &number);
+
+        if (number == 3)
+            printf("\nCongratulations, you guessed correctly!\n");
+        else
+            printf("\nSorry, still wrong.\n");
+    }
+    else {
+        printf("\n%d is not a valid option.\n", number);
+    }
+
+    return 0;
+}
+
 
